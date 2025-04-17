5a4a080733809da67148b95bcc126c3eebb44a88
SpecConSpeedTyping <antonio@speccon.co.za>
2025-04-17 11:40:03 +0200

    removed hero-image

--- client/src/assets/assets.js
+++ client/src/assets/assets.js
@@ -1,11 +1,9 @@
 import Spendy from './Spendy.svg';
-import bgVideo from './bg-video.mp4'; // Make sure this file exists!
-import heroImage from './hero-image.png'; // Or jpg, depending on your actual file
+import bgVideo from './bg-video.mp4'; // Make sure this file exists!// Or jpg, depending on your actual file
 
 const assets = {
     Spendy,
     bgVideo,
-    heroImage,
 };
 
 export default assets;
