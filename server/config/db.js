9b1678485a84e935b14ba1d16eb5d4bad121388c
SpecConSpeedTyping <antonio@speccon.co.za>
2025-04-17 13:28:23 +0200

    updated db.js to have es module

--- server/config/db.js
+++ server/config/db.js
@@ -1,11 +1,14 @@
-const mongoose = require("mongoose");
+import mongoose from "mongoose";
 
 const connectDB = async () => {
     try {
-    await mongoose.connect(process.env.MONGO_URI);
-    console.log("✅ Connected to MongoDB Atlas");
-    } catch (err) {
-    console.error("❌ MongoDB connection failed:", err);
+    await mongoose.connect(process.env.MONGO_URI, {
+        useNewUrlParser: true,
+        useUnifiedTopology: true,
+    });
+    console.log("✅ MongoDB connected successfully");
+    } catch (error) {
+    console.error("❌ MongoDB connection failed:", error.message);
     process.exit(1);
     }
 };
